ec5ca8c2a1333c48256d08d836d7fe650a1a9bee
Angelo7x <abennici96@gmail.com>
2021-10-06 15:39:40 +0200

    creata struttura

---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer>
+      <div class="footer-top"></div>
+      <div class="footer-bot"></div>
+  </footer>
+</template>
+
+<script>
+export default {
+    name: "Footer"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
